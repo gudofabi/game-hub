75efaefb30e7f095bfabe878e0bf47d57e8edc65
gudofabi <godofredocpanel@gmail.com>
2023-08-28 15:45:44 +0800

    fix: add badge

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -13,6 +13,7 @@ export interface Game {
   name: number;
   background_image: string;
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
